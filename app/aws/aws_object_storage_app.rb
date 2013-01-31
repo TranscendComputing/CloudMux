e742365c8aba3ceda0dc38eb3db6612beb72186a
bjones <bjones@transcendcomputing.com>
2013-01-31 17:39:59 -0600

    Added more object storage apis
    Former-commit-id: 83e8647942ef1ca6738d119765d4c9ad1bf5dd5c

--- app/aws/aws_object_storage_app.rb
+++ app/aws/aws_object_storage_app.rb
@@ -35,6 +35,84 @@ class AwsObjectStorageApp < ResourceApiBase
 			end
 		end
 	end
+	
+	delete '/directories/delete' do
+		object_storage = get_object_storage_interface(params[:cred_id])
+		if(object_storage.nil?)
+			[BAD_REQUEST]
+		else
+			json_body = body_to_json(request)
+			if(json_body.nil? || json_body["directory"].nil?)
+				[BAD_REQUEST]
+			else
+				response = object_storage.directories.get(json_body["directory"]["name"]).destroy
+				[OK, response.to_json]
+			end
+		end
+	end
+	
+	#
+	# Files
+	#
+	post '/directory/files' do
+		object_storage = get_object_storage_interface(params[:cred_id])
+		if(object_storage.nil?)
+			[BAD_REQUEST]
+		else
+			json_body = body_to_json(request)
+			if(json_body.nil? || json_body["directory"].nil?)
+				[BAD_REQUEST]
+			else
+				response = object_storage.directories.get(json_body["directory"]["name"]).files
+				[OK, response.to_json]
+			end
+		end
+	end
+	
+	post '/directory/file/download' do
+		object_storage = get_object_storage_interface(params[:cred_id])
+		if(object_storage.nil?)
+			[BAD_REQUEST]
+		else
+			json_body = body_to_json(request)
+			if(json_body.nil? || json_body["file"].nil?)
+				[BAD_REQUEST]
+			else
+				response = object_storage.get_object(json_body["file"]["directory_name"], json_body["file"]["name"]).body
+				[OK, response.to_json]
+			end
+		end
+	end
+	
+	post '/directory/file/upload' do
+		object_storage = get_object_storage_interface(params[:cred_id])
+		if(object_storage.nil?)
+			[BAD_REQUEST]
+		else
+			json_body = body_to_json(request)
+			if(json_body.nil? || json_body["file"].nil?)
+				[BAD_REQUEST]
+			else
+				response = object_storage.put_object(json_body["file"]["directory_name"], json_body["file"]["name"], json_body["file"]["body"])
+				[OK, response.to_json]
+			end
+		end
+	end
+	
+	delete '/directory/file/delete' do
+		object_storage = get_object_storage_interface(params[:cred_id])
+		if(object_storage.nil?)
+			[BAD_REQUEST]
+		else
+			json_body = body_to_json(request)
+			if(json_body.nil? || json_body["file"].nil?)
+				[BAD_REQUEST]
+			else
+				response = object_storage.delete_object(json_body["file"]["directory_name"], json_body["file"]["name"])
+				[OK, response.to_json]
+			end
+		end
+	end
 
 	def get_object_storage_interface(cred_id)
 		if(cred_id.nil?)
