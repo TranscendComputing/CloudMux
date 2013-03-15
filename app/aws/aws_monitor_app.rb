020087813541128801bd6a6bee899c0ab0387a03
Curtis Stewart <cstewart@momentumsi.com>
2013-03-15 09:48:23 -0500

    Added support for cloud accounts vs cloud credentials.  Added base openstack compute api app.

--- app/aws/aws_monitor_app.rb
+++ app/aws/aws_monitor_app.rb
@@ -113,7 +113,7 @@ class AwsMonitorApp < ResourceApiBase
 		if(cred_id.nil?)
 			return nil
 		else
-			cloud_cred = Account.find_cloud_account(cred_id)
+			cloud_cred = get_creds(cred_id)
 			if cloud_cred.nil?
 				return nil
 			else
--- app/cloud_account_api_app.rb
+++ app/cloud_account_api_app.rb
@@ -1,6 +1,30 @@
 require 'sinatra'
 
 class CloudAccountApiApp < ApiBase
+  get '/' do
+    if params[:org_id].nil?
+      message = Error.new.extend(ErrorRepresenter)
+      message.message = "Must provide org_id with request for cloud accounts."
+      [BAD_REQUEST, message.to_json]
+    elsif params[:account_id].nil?
+      message = Error.new.extend(ErrorRepresenter)
+      message.message = "Must provide account_id with request for cloud accounts."
+      [BAD_REQUEST, message.to_json]
+    end
+
+    per_page = (params[:per_page] || 20).to_i
+    page = (params[:page] || 1).to_i
+    offset = (page-1)*per_page
+    conditions = { }
+    conditions[:org_id] = params[:org_id]
+    conditions[:cloud_id] = params[:cloud_id] if params[:cloud_id]
+    cloud_accounts = CloudAccount.all.where(conditions)
+    count = CloudAccount.where(conditions).count
+    query = Query.new(count, page, offset, per_page)
+    cloud_account_query = CloudAccountQuery.new(query, cloud_accounts).extend(CloudAccountQueryRepresenter)
+    [OK, cloud_account_query.to_json]
+  end
+
   get '/:id.json' do
     cloud_account = CloudAccount.find(params[:id])
     cloud_account.extend(CloudAccountRepresenter)
@@ -8,112 +32,112 @@ class CloudAccountApiApp < ApiBase
   end
 
   post '/' do
-    new_cloud_accout = CloudAccount.new.extend(UpdateCloudAccountRepresenter)
-    new_cloud_accout.from_json(request.body.read)
-    if new_cloud_accout.valid?
-      new_cloud_accout.save!
+    new_cloud_account = CloudAccount.new.extend(UpdateCloudAccountRepresenter)
+    new_cloud_account.from_json(request.body.read)
+    if new_cloud_account.valid?
+      new_cloud_account.save!
       # refresh without the Update representer, so that we don't serialize private data back
-      cloud_accout = CloudAccount.find(new_cloud_accout.id).extend(CloudAccountRepresenter)
-      [CREATED, cloud_accout.to_json]
+      cloud_account = CloudAccount.find(new_cloud_account.id).extend(CloudAccountRepresenter)
+      [CREATED, cloud_account.to_json]
     else
       message = Error.new.extend(ErrorRepresenter)
-      message.message = "#{new_cloud_accout.errors.full_messages.join(";")}"
-      message.validation_errors = new_cloud_accout.errors.to_hash
+      message.message = "#{new_cloud_account.errors.full_messages.join(";")}"
+      message.validation_errors = new_cloud_account.errors.to_hash
       [BAD_REQUEST, message.to_json]
     end
   end
 
   put '/:id' do
-    update_cloud_accout = CloudAccount.find(params[:id])
-    update_cloud_accout.extend(UpdateCloudAccountRepresenter)
-    update_cloud_accout.from_json(request.body.read)
-    if update_cloud_accout.valid?
-      update_cloud_accout.save!
+    update_cloud_account = CloudAccount.find(params[:id])
+    update_cloud_account.extend(UpdateCloudAccountRepresenter)
+    update_cloud_account.from_json(request.body.read)
+    if update_cloud_account.valid?
+      update_cloud_account.save!
       # refresh without the Update representer, so that we don't serialize the password data back across
-      cloud_accout = CloudAccount.find(update_cloud_accout.id).extend(CloudAccountRepresenter)
-      [OK, cloud_accout.to_json]
+      cloud_account = CloudAccount.find(update_cloud_account.id).extend(CloudAccountRepresenter)
+      [OK, cloud_account.to_json]
     else
       message = Error.new.extend(ErrorRepresenter)
-      message.message = "#{update_cloud_accout.errors.full_messages.join(";")}"
-      message.validation_errors = update_cloud_accout.errors.to_hash
+      message.message = "#{update_cloud_account.errors.full_messages.join(";")}"
+      message.validation_errors = update_cloud_account.errors.to_hash
       [BAD_REQUEST, message.to_json]
     end
   end
 
   delete '/:id' do
-      cloud_accout = CloudAccount.find(params[:id])
-      cloud_accout.delete
+      cloud_account = CloudAccount.find(params[:id])
+      cloud_account.delete
       [OK]
   end
 
-  # Register a new service to an existing cloud_accout
+  # Register a new service to an existing cloud_account
   post '/:id/services' do
-    update_cloud_accout = CloudAccount.find(params[:id])
+    update_cloud_account = CloudAccount.find(params[:id])
     new_service = CloudService.new.extend(UpdateCloudServiceRepresenter)
     new_service.from_json(request.body.read)
-    new_service.cloud_accout = update_cloud_accout
+    new_service.cloud_account = update_cloud_account
     new_service.save!
-    cloud_accout = CloudAccount.find(update_cloud_accout.id).extend(CloudAccountRepresenter)
-    update_cloud_accout.extend(CloudAccountRepresenter)
-    [OK, update_cloud_accout.to_json]
+    cloud_account = CloudAccount.find(update_cloud_account.id).extend(CloudAccountRepresenter)
+    update_cloud_account.extend(CloudAccountRepresenter)
+    [OK, update_cloud_account.to_json]
   end
 
-  # Remove a service from an existing cloud_accout
+  # Remove a service from an existing cloud_account
   delete '/:id/services/:service_id' do
-    update_cloud_accout = CloudAccount.find(params[:id])
-    update_cloud_accout.remove_service!(params[:service_id])
-    cloud_accout = CloudAccount.find(update_cloud_accout.id).extend(CloudAccountRepresenter)
-    update_cloud_accout.extend(CloudAccountRepresenter)
-    [OK, update_cloud_accout.to_json]
+    update_cloud_account = CloudAccount.find(params[:id])
+    update_cloud_account.remove_service!(params[:service_id])
+    cloud_account = CloudAccount.find(update_cloud_account.id).extend(CloudAccountRepresenter)
+    update_cloud_account.extend(CloudAccountRepresenter)
+    [OK, update_cloud_account.to_json]
   end
 
-  # Register a new mapping to an existing cloud_accout
+  # Register a new mapping to an existing cloud_account
   post '/:id/mappings' do
-    update_cloud_accout = CloudAccount.find(params[:id])
+    update_cloud_account = CloudAccount.find(params[:id])
     new_mapping = CloudMapping.new.extend(UpdateCloudMappingRepresenter)
     new_mapping.properties = { }
     new_mapping.mapping_entries = []
     new_mapping.from_json(request.body.read)
-    new_mapping.mappable = update_cloud_accout
+    new_mapping.mappable = update_cloud_account
     new_mapping.save!
-    update_cloud_accout.extend(CloudAccountRepresenter)
-    [OK, update_cloud_accout.to_json]
+    update_cloud_account.extend(CloudAccountRepresenter)
+    [OK, update_cloud_account.to_json]
   end
 
-  # Remove a mapping from an existing cloud_accout
+  # Remove a mapping from an existing cloud_account
   delete '/:id/mappings/:mapping_id' do
-    update_cloud_accout = CloudAccount.find(params[:id])
-    update_cloud_accout.remove_mapping!(params[:mapping_id])
-    update_cloud_accout.extend(CloudAccountRepresenter)
-    [OK, update_cloud_accout.to_json]
+    update_cloud_account = CloudAccount.find(params[:id])
+    update_cloud_account.remove_mapping!(params[:mapping_id])
+    update_cloud_account.extend(CloudAccountRepresenter)
+    [OK, update_cloud_account.to_json]
   end
   
-  # Register a new compute price to an existing cloud_accout
+  # Register a new compute price to an existing cloud_account
   post '/:id/prices' do
-    update_cloud_accout = CloudAccount.find(params[:id])
+    update_cloud_account = CloudAccount.find(params[:id])
     new_price = Price.new.extend(UpdatePriceRepresenter)
     new_price.properties = { }
     new_price.entries = []
     new_price.from_json(request.body.read)
-    new_price.cloud_accout = update_cloud_accout
+    new_price.cloud_account = update_cloud_account
     new_price.save!
-    update_cloud_accout.extend(CloudAccountRepresenter)
-    [OK, update_cloud_accout.to_json]
+    update_cloud_account.extend(CloudAccountRepresenter)
+    [OK, update_cloud_account.to_json]
   end
   
-  # Remove a price from an existing cloud_accout
+  # Remove a price from an existing cloud_account
   delete '/:id/prices/:price_id' do
-    update_cloud_accout = CloudAccount.find(params[:id])
-    update_cloud_accout.remove_price!(params[:price_id])
-    cloud_accout = CloudAccount.find(update_cloud_accout.id).extend(CloudAccountRepresenter)
-    update_cloud_accout.extend(CloudAccountRepresenter)
-    [OK, update_cloud_accout.to_json]
+    update_cloud_account = CloudAccount.find(params[:id])
+    update_cloud_account.remove_price!(params[:price_id])
+    cloud_account = CloudAccount.find(update_cloud_account.id).extend(CloudAccountRepresenter)
+    update_cloud_account.extend(CloudAccountRepresenter)
+    [OK, update_cloud_account.to_json]
   end
   
   # Update a price effective price and date, and add to entries
   put '/:id/prices/:price_id' do
-    update_cloud_accout = CloudAccount.find(params[:id])
-    update_price = update_cloud_accout.find_price(params[:price_id])
+    update_cloud_account = CloudAccount.find(params[:id])
+    update_price = update_cloud_account.find_price(params[:price_id])
     if update_price.nil?
       return [NOT_FOUND]
     end
@@ -121,7 +145,7 @@ class CloudAccountApiApp < ApiBase
     update_price.from_json(request.body.read)
     if update_price.valid?
       update_price.save!
-      updated_price = update_cloud_accout.find_price(update_price.id)
+      updated_price = update_cloud_account.find_price(update_price.id)
       updated_price.extend(PriceRepresenter)
       [OK, updated_price.to_json]
     else
