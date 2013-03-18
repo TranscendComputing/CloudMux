5517d48bb1ac3ce6ad2b2fab992a811bccdffdfe
Barrett <barrett@barrett-VirtualBox.(none)>
2013-03-18 14:15:11 -0500

    corrected get_dns_interface function

--- app/aws/aws_dns_app.rb
+++ app/aws/aws_dns_app.rb
@@ -129,7 +129,7 @@ class AwsDnsApp < ResourceApiBase
 		if(cred_id.nil?)
 			return nil
 		else
-			cloud_cred = Account.find_cloud_account(cred_id)
+			cloud_cred = get_creds(cred_id)
 			if cloud_cred.nil?
 				return nil
 			else
