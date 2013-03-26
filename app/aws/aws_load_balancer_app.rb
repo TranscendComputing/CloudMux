c93269a0b37af358f380d8d198ac8e23bb56989d
Barrett <barretthjones@gmail.com>
2013-03-26 17:37:01 -0500

    additional load balancer apis

--- app/aws/aws_load_balancer_app.rb
+++ app/aws/aws_load_balancer_app.rb
@@ -82,6 +82,184 @@ class AwsLoadBalancerApp < ResourceApiBase
 		end
 	end
 
+	post '/load_balancers/availability_zones/enable' do
+		elb = get_load_balancer_interface(params[:cred_id], params[:region])
+		if(elb.nil?)
+			[BAD_REQUEST]
+		else
+			json_body = body_to_json(request)
+			if(json_body.nil? || json_body["availability_zones"].nil? || json_body["id"].nil?)
+				[BAD_REQUEST]
+			else
+				begin
+					response = elb.enable_availability_zones_for_load_balancer(json_body["availability_zones"], json_body["id"])
+					[OK, response.to_json]
+				rescue => error
+					handle_error(error)
+				end
+			end
+		end
+	end
+
+	post '/load_balancers/availability_zones/disable' do
+		elb = get_load_balancer_interface(params[:cred_id], params[:region])
+		if(elb.nil?)
+			[BAD_REQUEST]
+		else
+			json_body = body_to_json(request)
+			if(json_body.nil? || json_body["availability_zones"].nil? || json_body["id"].nil?)
+				[BAD_REQUEST]
+			else
+				begin
+					response = elb.disable_availability_zones_for_load_balancer(json_body["availability_zones"], json_body["id"])
+					[OK, response.to_json]
+				rescue => error
+					handle_error(error)
+				end
+			end
+		end
+	end
+
+	post '/load_balancers/instances/register' do
+		elb = get_load_balancer_interface(params[:cred_id], params[:region])
+		if(elb.nil?)
+			[BAD_REQUEST]
+		else
+			json_body = body_to_json(request)
+			if(json_body.nil? || json_body["instance_ids"].nil? || json_body["id"].nil?)
+				[BAD_REQUEST]
+			else
+				begin
+					response = elb.register_instances_with_load_balancer(json_body["instance_ids"], json_body["id"])
+					[OK, response.to_json]
+				rescue => error
+					handle_error(error)
+				end
+			end
+		end
+	end
+
+	post '/load_balancers/instances/deregister' do
+		elb = get_load_balancer_interface(params[:cred_id], params[:region])
+		if(elb.nil?)
+			[BAD_REQUEST]
+		else
+			json_body = body_to_json(request)
+			if(json_body.nil? || json_body["instance_ids"].nil? || json_body["id"].nil?)
+				[BAD_REQUEST]
+			else
+				begin
+					response = elb.deregister_instances_from_load_balancer(json_body["instance_ids"], json_body["id"])
+					[OK, response.to_json]
+				rescue => error
+					handle_error(error)
+				end
+			end
+		end
+	end
+
+	post '/load_balancers/instances/available' do
+		
+	end
+
+	get '/load_balancers/describe_health' do
+		elb = get_load_balancer_interface(params[:cred_id], params[:region])
+		if(elb.nil? || params[:availability_zones].nil? || params[:id].nil?)
+			[BAD_REQUEST]
+		else
+			begin
+				availability_zones_health = []
+				availability_zones = JSON.parse(params[:availability_zones])
+				availability_zones.each do |az|
+					az_health = {}
+					az_health["LoadBalancerName"] = params[:id]
+					az_health["AvailabilityZone"] = az
+					az_health["InstanceCount"] = 0
+					az_health["Healthy"] = false
+					availability_zones_health << az_health
+				end
+				instance_health = elb.describe_instance_health(params[:id]).body["DescribeInstanceHealthResult"]["InstanceStates"]
+				compute = get_compute_interface(params[:cred_id], params[:region])
+				instance_health.each do |i|
+					instance = compute.servers.get(i["InstanceId"])
+					i["AvailabilityZone"] = instance.availability_zone
+					availability_zones_health.each do |a|
+						if a["AvailabilityZone"] == i["AvailabilityZone"]
+							a["InstanceCount"] = a["InstanceCount"] + 1
+							if i["State"] == "InService"
+								a["Healthy"] = true
+							end
+						end
+					end
+				end
+				response = {"AvailabilityZonesHealth" => availability_zones_health, "InstancesHealth" => instance_health}
+				[OK, response.to_json]
+			rescue => error
+				handle_error(error)
+			end
+		end
+	end
+
+	#
+	# Load Balancer Listeners
+	#
+	get '/load_balancers/listeners/describe' do
+		elb = get_load_balancer_interface(params[:cred_id], params[:region])
+		if(elb.nil?)
+			[BAD_REQUEST]
+		else
+			load_balancer = params[:load_balancer]
+			if(load_balancer.nil?)
+				[BAD_REQUEST]
+			else
+				begin
+					response = elb.load_balancers.get(load_balancer).listeners
+					[OK, response.to_json]
+				rescue => error
+					handle_error(error)
+				end
+			end
+		end
+	end
+
+	put '/load_balancers/listeners/create' do
+		elb = get_load_balancer_interface(params[:cred_id], params[:region])
+		if(elb.nil?)
+			[BAD_REQUEST]
+		else
+			json_body = body_to_json(request)
+			if(json_body.nil? || json_body["load_balancer"].nil?)
+				[BAD_REQUEST]
+			else
+				begin
+					response = elb.create_load_balancer_listeners(json_body["load_balancer"]["id"], json_body["load_balancer"]["listeners"])
+					[OK, response.to_json]
+				rescue => error
+					handle_error(error)
+				end
+			end
+		end
+	end
+
+	delete '/load_balancers/listeners/delete' do
+		elb = get_load_balancer_interface(params[:cred_id], params[:region])
+		if(elb.nil?)
+			[BAD_REQUEST]
+		else
+			json_body = body_to_json(request)
+			if(json_body.nil? || json_body["load_balancer"].nil?)
+				[BAD_REQUEST]
+			else
+				begin
+					response = elb.delete_load_balancer_listeners(json_body["load_balancer"]["id"], json_body["load_balancer"]["ports"])
+					[OK, response.to_json]
+				rescue => error
+					handle_error(error)
+				end
+			end
+		end
+	end
+
 	def get_load_balancer_interface(cred_id, region)
 		if(cred_id.nil?)
 			return nil
@@ -94,6 +272,23 @@ class AwsLoadBalancerApp < ResourceApiBase
 					return Fog::AWS::ELB.new({:aws_access_key_id => cloud_cred.access_key, :aws_secret_access_key => cloud_cred.secret_key})
 				else
 					return Fog::AWS::ELB.new({:aws_access_key_id => cloud_cred.access_key, :aws_secret_access_key => cloud_cred.secret_key, :region => region})
+				end
+			end
+		end
+	end
+
+	def get_compute_interface(cred_id, region)
+		if(cred_id.nil?)
+			return nil
+		else
+			cloud_cred = get_creds(cred_id)
+			if cloud_cred.nil?
+				return nil
+			else
+				if region.nil? or region == "undefined" or region == ""
+					return Fog::Compute::AWS.new({:aws_access_key_id => cloud_cred.access_key, :aws_secret_access_key => cloud_cred.secret_key})
+				else
+					return Fog::Compute::AWS.new({:aws_access_key_id => cloud_cred.access_key, :aws_secret_access_key => cloud_cred.secret_key, :region => region})
 				end
 			end
 		end
