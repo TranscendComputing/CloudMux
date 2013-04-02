3a539d5a0078d80b79a74f141d38bffeb61fde48
Curtis Stewart <cstewart@momentumsi.com>
2013-04-02 10:26:08 -0500

    Added support for editing/adding/removing users in a tenant.

--- app/openstack/openstack_identity_app.rb
+++ app/openstack/openstack_identity_app.rb
@@ -58,6 +58,24 @@ class OpenstackIdentityApp < ResourceApiBase
 			handle_error(error)
 		end
 	end
+
+    # Get roles for users in tenant
+    get '/users/:id/:tenant_id/roles' do
+        begin
+            user_roles = @identity.list_roles_for_user_on_tenant(params[:tenant_id], params[:id]).body["roles"]
+            all_roles = @identity.list_roles.body["roles"]
+            available_roles = []
+            all_roles.select do |role|
+                unless user_roles.detect {|r| r["id"] == role["id"]}
+                    available_roles << role
+                end
+            end
+            response = {:available_roles => available_roles, :roles => user_roles}
+            [OK, response.to_json]
+        rescue => error
+            handle_error(error)
+        end
+    end
 	
 	#
 	# Tenants
@@ -94,15 +112,41 @@ class OpenstackIdentityApp < ResourceApiBase
         end
     end
 
-    # Completely removes user from tenant 
+    # Completely removes user from tenant if no specific roles are passed
+    # Otherwise, removes role from user on tenant
     delete '/tenants/:id/users/:user_id' do
-        begin
-            roles = @identity.list_roles_for_user_on_tenant(params[:id], params[:user_id]).body["roles"]
-            roles.each {|role| @identity.remove_user_from_tenant(params[:id], params[:user_id], role["id"])}
-            response = @identity.list_users(params[:id]).body["users"]
-            [OK, response.to_json]
-        rescue => error
-            handle_error(error)
+        json_body = body_to_json(request)
+        if(json_body.nil? || json_body["role_id"].nil?)
+            begin
+                roles = @identity.list_roles_for_user_on_tenant(params[:id], params[:user_id]).body["roles"]
+                roles.each {|role| @identity.remove_user_from_tenant(params[:id], params[:user_id], role["id"])}
+                response = @identity.list_users(params[:id]).body["users"]
+                [OK, response.to_json]
+            rescue => error
+                handle_error(error)
+            end
+        else
+            begin
+                response = @identity.remove_user_from_tenant(params[:id], params[:user_id], json_body["role_id"]).body
+                [OK, response.to_json]
+            rescue => error
+                handle_error(error)
+            end
+        end
+    end
+
+    # Add role for user to tenant
+    post '/tenants/:id/users/:user_id' do
+        json_body = body_to_json(request)
+        if(json_body.nil? || json_body["role_id"].nil?)
+            [BAD_REQUEST]
+        else
+            begin
+                response = @identity.add_user_to_tenant(params[:id], params[:user_id], json_body["role_id"]).body
+                [OK, response.to_json]
+            rescue => error
+                handle_error(error)
+            end
         end
     end
 
