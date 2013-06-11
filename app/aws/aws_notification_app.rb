5dc0b003b0c702d56d6acbbc0c4b6edf3fd2124a
Barrett <barretthjones@gmail.com>
2013-06-11 17:05:23 -0500

    added create and delete apis for sns

--- app/aws/aws_notification_app.rb
+++ app/aws/aws_notification_app.rb
@@ -5,7 +5,7 @@ class AwsNotificationApp < ResourceApiBase
 	#
 	# Topics
 	#
-	get '/topics/describe' do
+	get '/topics/list' do
 		notification = get_notification_interface(params[:cred_id], params[:region])
 		if(notification.nil?)
 			[BAD_REQUEST]
@@ -22,6 +22,70 @@ class AwsNotificationApp < ResourceApiBase
 		end
 	end
 
+	get '/topics/describe' do
+		notification = get_notification_interface(params[:cred_id], params[:region])
+		if(notification.nil?)
+			[BAD_REQUEST]
+		else
+			filters = params[:filters]
+			if(filters.nil?)
+				raw_response = notification.list_topics.body["Topics"]
+			else
+				raw_response = notification.list_topics(filters).body["Topics"]
+			end
+			response = []
+			raw_response.each do |t|
+				topic = {}
+				topic["id"] = t
+				begin
+					topic["Name"] = t.split(":").last
+					attributes = notification.get_topic_attributes(t).body["Attributes"]
+					topic.merge!(attributes)
+				rescue
+				end
+				response.push(topic)
+			end
+			[OK, response.to_json]
+		end
+	end
+
+	put '/topics/create' do
+		notification = get_notification_interface(params[:cred_id], params[:region])
+		if(notification.nil?)
+			[BAD_REQUEST]
+		else
+			json_body = body_to_json(request)
+			if(json_body.nil?)
+				[BAD_REQUEST]
+			else
+				begin
+					response = notification.create_topic(json_body["topic"]["name"])
+					[OK, response.to_json]
+				rescue => error
+					handle_error(error)
+				end
+			end
+		end
+	end
+
+	delete '/topics/delete' do
+		notification = get_notification_interface(params[:cred_id], params[:region])
+		if(notification.nil?)
+			[BAD_REQUEST]
+		else
+			json_body = body_to_json(request)
+			if(json_body.nil? || json_body["topic"].nil?)
+				[BAD_REQUEST]
+			else
+				begin
+					response = notification.delete_topic(json_body["topic"]["id"])
+					[OK, response.to_json]
+				rescue => error
+					handle_error(error)
+				end
+			end
+		end
+	end
 
 	def get_notification_interface(cred_id, region)
 		if(cred_id.nil?)
