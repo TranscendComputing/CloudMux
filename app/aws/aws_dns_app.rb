31b5a4c94f84e890b9437f6f02d537a003d53ef8
Barrett <barretthjones@gmail.com>
2013-06-13 11:21:29 -0500

    refactored aws dns to be more RESTful

--- app/aws/aws_dns_app.rb
+++ app/aws/aws_dns_app.rb
@@ -2,138 +2,82 @@ require 'sinatra'
 require 'fog'
 
 class AwsDnsApp < ResourceApiBase
+
+	before do
+		if ! params[:cred_id].nil?
+			cloud_cred = get_creds(params[:cred_id])
+			if ! cloud_cred.nil?
+				@dns = Fog::DNS::AWS.new({:aws_access_key_id => cloud_cred.access_key, :aws_secret_access_key => cloud_cred.secret_key})
+			end
+		end
+		halt [BAD_REQUEST] if @dns.nil?
+    end
+
 	#
 	# Hosted Zones
 	#
-	get '/hosted_zones/describe' do
-		dns = get_dns_interface(params[:cred_id])
-		if(dns.nil?)
+	get '/hosted_zones' do
+		filters = params[:filters]
+		if(filters.nil?)
+			response = @dns.zones
+		else
+			response = @dns.zones.all(filters)
+		end
+		[OK, response.to_json]
+	end
+	
+	post '/hosted_zones' do
+		json_body = body_to_json(request)
+		if(json_body.nil?)
 			[BAD_REQUEST]
 		else
-			filters = params[:filters]
-			if(filters.nil?)
-				response = dns.zones
-			else
-				response = dns.zones.all(filters)
+			begin
+				response = @dns.zones.create(json_body["hosted_zone"])
+				[OK, response.to_json]
+			rescue => error
+				handle_error(error)
 			end
+		end
+	end
+	
+	delete '/hosted_zones/:id' do
+		begin
+			response = @dns.zones.get(params[:id]).destroy
 			[OK, response.to_json]
-		end
-	end
-	
-	put '/hosted_zones/create' do
-		dns = get_dns_interface(params[:cred_id])
-		if(dns.nil?)
-			[BAD_REQUEST]
-		else
-			json_body = body_to_json(request)
-			if(json_body.nil?)
-				[BAD_REQUEST]
-			else
-				begin
-					response = dns.zones.create(json_body["hosted_zone"])
-					[OK, response.to_json]
-				rescue => error
-					handle_error(error)
-				end
-			end
-		end
-	end
-	
-	delete '/hosted_zones/delete' do
-		dns = get_dns_interface(params[:cred_id])
-		if(dns.nil?)
-			[BAD_REQUEST]
-		else
-			json_body = body_to_json(request)
-			if(json_body.nil? || json_body["hosted_zone"].nil?)
-				[BAD_REQUEST]
-			else
-				begin
-					response = dns.zones.get(json_body["hosted_zone"]["id"]).destroy
-					[OK, response.to_json]
-				rescue => error
-					handle_error(error)
-				end
-			end
+		rescue => error
+			handle_error(error)
 		end
 	end
 
 	#
 	# Record Sets
 	#
-	get '/record_sets/describe' do
-		dns = get_dns_interface(params[:cred_id])
-		if(dns.nil?)
-			[BAD_REQUEST]
-		else
-			hosted_zone_id = params[:hosted_zone_id]
-			if(hosted_zone_id.nil?)
-				[BAD_REQUEST]
-			else
-				begin
-					response = dns.list_resource_record_sets(hosted_zone_id).body["ResourceRecordSets"]
-					[OK, response.to_json]
-				rescue => error
-					handle_error(error)
-				end
-			end
+	get '/hosted_zones/:hosted_zone_id/record_sets' do
+		begin
+			response = @dns.list_resource_record_sets(params[:hosted_zone_id]).body["ResourceRecordSets"]
+			[OK, response.to_json]
+		rescue => error
+			handle_error(error)
 		end
 	end
 	
-	put '/record_sets/change' do
-		dns = get_dns_interface(params[:cred_id])
-		if(dns.nil?)
+	put '/hosted_zones/:hosted_zone_id/record_sets/change' do
+		json_body = body_to_json(request)
+		if(json_body.nil?)
 			[BAD_REQUEST]
 		else
-			json_body = body_to_json(request)
-			if(json_body.nil?)
-				[BAD_REQUEST]
-			else
-				begin
-					#must symbolize keys
-					change_batch_symbolized = []
-					json_body["record_set"]["change_batch"].each {|change| change_batch_symbolized<<change.symbolize_keys}
-					if(json_body["record_set"]["options"].nil?)
-						response = dns.change_resource_record_sets(json_body["record_set"]["hosted_zone_id"], change_batch_symbolized)
-					else
-						response = dns.change_resource_record_sets(json_body["record_set"]["hosted_zone_id"], change_batch_symbolized, json_body["record_set"]["options"])
-					end
-					[OK, response.to_json]
-				rescue => error
-					handle_error(error)
+			begin
+				#must symbolize keys
+				change_batch_symbolized = []
+				json_body["record_set"]["change_batch"].each {|change| change_batch_symbolized<<change.symbolize_keys}
+				if(json_body["record_set"]["options"].nil?)
+					response = @dns.change_resource_record_sets(params[:hosted_zone_id], change_batch_symbolized)
+				else
+					response = @dns.change_resource_record_sets(params[:hosted_zone_id], change_batch_symbolized, json_body["record_set"]["options"])
 				end
-			end
-		end
-	end
-	
-	delete '/record_sets/delete' do
-		dns = get_dns_interface(params[:cred_id])
-		if(dns.nil?)
-			[BAD_REQUEST]
-		else
-			json_body = body_to_json(request)
-			if(json_body.nil? || json_body["hosted_zone"].nil?)
-				[BAD_REQUEST]
-			else
-				begin
-					response = dns.zones.get(json_body["hosted_zone"]["id"]).destroy
-					[OK, response.to_json]
-				rescue => error
-					handle_error(error)
-				end
-			end
-		end
-	end
-
-	def get_dns_interface(cred_id)
-		if(cred_id.nil?)
-			return nil
-		else
-			cloud_cred = get_creds(cred_id)
-			if cloud_cred.nil?
-				return nil
-			else
-				return Fog::DNS::AWS.new({:aws_access_key_id => cloud_cred.access_key, :aws_secret_access_key => cloud_cred.secret_key})
+				[OK, response.to_json]
+			rescue => error
+				handle_error(error)
 			end
 		end
 	end
