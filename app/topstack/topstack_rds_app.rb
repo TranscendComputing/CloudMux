00990713e7c831982b3aebb5cbff4e1c4e23ff78
Barrett <barretthjones@gmail.com>
2013-06-24 16:48:39 -0500

    added support for topstack rds apis
    Former-commit-id: 0c9133d7faa6aee3d5e22795f2450d175d3bb4cc

---
+++ app/topstack/topstack_rds_app.rb
@@ -0,0 +1,98 @@
+require 'sinatra'
+require 'fog'
+
+class TopStackRdsApp < ResourceApiBase
+
+	before do
+		if(params[:cred_id].nil?)
+            halt [BAD_REQUEST]
+        else
+            cloud_cred = get_creds(params[:cred_id])
+            if cloud_cred.nil?
+                halt [NOT_FOUND, "Credentials not found."]
+            else
+                begin
+                    # Find Monitor service endpoint
+                    endpoint = cloud_cred.cloud_account.cloud_services.where({"service_type"=>"RDS"}).first
+                    halt [BAD_REQUEST] if endpoint.nil?
+                    fog_options = {:aws_access_key_id => cloud_cred.access_key, :aws_secret_access_key => cloud_cred.secret_key}
+                    fog_options.merge!(:host => endpoint[:host], :port => endpoint[:port], :path => endpoint[:path], :scheme => endpoint[:protocol])
+                    @rds = Fog::AWS::RDS.new(fog_options)
+                    halt [BAD_REQUEST] if @rds.nil?
+                rescue Fog::Errors::NotFound => error
+                    halt [NOT_FOUND, error.to_s]
+                end
+            end
+        end
+    end
+
+	#
+	# Databases
+	#
+	get '/databases' do
+		filters = params[:filters]
+		if(filters.nil?)
+			response = @rds.servers
+		else
+			response = @rds.servers.all(filters)
+		end
+		[OK, response.to_json]
+	end
+	
+	post '/databases' do
+		json_body = body_to_json(request)
+		if(json_body.nil?)
+			[BAD_REQUEST]
+		else
+			begin
+				response = @rds.servers.create(json_body["relational_database"])
+				[OK, response.to_json]
+			rescue => error
+				handle_error(error)
+			end
+		end
+	end
+	
+	delete '/databases/:id' do
+		begin
+			response = @rds.servers.get(params[:id]).destroy
+			[OK, response.to_json]
+		rescue => error
+			handle_error(error)
+		end
+	end
+
+	get '/engine_versions' do
+		begin
+			engine_versions = @rds.describe_db_engine_versions.body['DescribeDBEngineVersionsResult']['DBEngineVersions'].as_json
+			engine_versions.each do |v|
+				v.each_pair do |name, value|
+					v[name] = value.strip
+				end
+			end
+			[OK, engine_versions.to_json]
+		rescue => error
+			handle_error(error)
+		end
+	end
+
+	get '/parameter_groups' do
+		filters = params[:filters]
+		if(filters.nil?)
+			response = @rds.parameter_groups
+		else
+			response = @rds.parameter_groups.all(filters)
+		end
+		[OK, response.to_json]
+	end
+
+	get '/security_groups' do
+		filters = params[:filters]
+		if(filters.nil?)
+			response = @rds.security_groups
+		else
+			response = @rds.security_groups.all(filters)
+		end
+		[OK, response.to_json]
+	end
+end
