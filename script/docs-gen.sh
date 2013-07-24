61b6ba196cfe12620b6b1f11a371d02ac76795b4
John Gardner <jgardner@transcendcomputing.com>
2013-07-24 15:31:04 -0500

    Bundle exec source2swagger.

--- script/docs-gen.sh
+++ script/docs-gen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-source2swagger -c "##~" -o docs -f config.ru || exit 1
-source2swagger -c "##~" -o docs -i app -e "rb" || exit 2
+bundle exec source2swagger -c "##~" -o docs -f config.ru || exit 1
+bundle exec source2swagger -c "##~" -o docs -i app -e "rb" || exit 2
