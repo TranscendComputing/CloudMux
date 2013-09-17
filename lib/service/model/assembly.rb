61358bf14863a8c0cdbcefa41b8567fc122367e1
Adan Saenz <asaenz@momentumsi.com>
2013-09-17 15:30:39 -0500

    Added fields to assembly model.

--- lib/service/model/assembly.rb
+++ lib/service/model/assembly.rb
@@ -5,10 +5,13 @@ class Assembly
   field :name, type:String
   field :cloud, type:String
   field :configurations, type:Hash
+  field :tool, type:String
+  field :cloud_credential, type:String
+  field :image, type:Hash
   belongs_to :account, :foreign_key=> 'account_id'
 
   attr_readonly :account_id
-  attr_accessible :name, :cloud, :configurations, :account_id
+  attr_accessible :name, :cloud, :configurations, :account_id, :tool, :cloud_credential, :image
 
   #has_one :image
 
