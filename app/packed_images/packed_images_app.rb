d7e4151598a0928b22aee5c67c45f1650d4b8203
Dev Mohanty <dmohanty09@austincollege.edu>
2013-09-25 16:48:57 -0500

    added calls to packed images

--- app/packed_images/packed_images_app.rb
+++ app/packed_images/packed_images_app.rb
@@ -8,4 +8,32 @@ class PackedImagesApiApp < ApiBase
         response = http.request(Net::HTTP::Get.new(uri.request_uri))
         [OK, response.body]
     end
+    
+    get '/builders' do
+        uri = URI.parse("http://localhost:8080/templates/builders")
+        http = Net::HTTP.new(uri.host, uri.port)
+        response = http.request(Net::HTTP::Get.new(uri.request_uri))
+        [OK, response.body]
+    end
+    
+    get '/builders/:id' do
+        uri = URI.parse("http://localhost:8080/templates/builders/"+params[:id])
+        http = Net::HTTP.new(uri.host, uri.port)
+        response = http.request(Net::HTTP::Get.new(uri.request_uri))
+        [OK, response.body]
+    end
+    
+    get '/provisioners' do
+        uri = URI.parse("http://localhost:8080/templates/provisioners")
+        http = Net::HTTP.new(uri.host, uri.port)
+        response = http.request(Net::HTTP::Get.new(uri.request_uri))
+        [OK, response.body]
+    end
+    
+    get '/provisioners/:id' do
+        uri = URI.parse("http://localhost:8080/templates/provisioners/"+params[:id])
+        http = Net::HTTP.new(uri.host, uri.port)
+        response = http.request(Net::HTTP::Get.new(uri.request_uri))
+        [OK, response.body]
+    end
 end
