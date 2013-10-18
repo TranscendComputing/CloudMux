43c509c2315c7b3203badbd59b822833d77be703
Dev Mohanty <dmohanty09@austincollege.edu>
2013-10-18 11:53:20 -0500

    changed endpoint for mciaas

--- app/packed_images/packed_images_app.rb
+++ app/packed_images/packed_images_app.rb
@@ -4,49 +4,49 @@ require 'httparty'
 
 class PackedImagesApiApp < ApiBase
     get '/' do
-        uri = URI.parse("http://localhost:9090/templates/builders")
+        uri = URI.parse("http://172.31.254.6:8080/templates/builders")
         http = Net::HTTP.new(uri.host, uri.port)
         response = http.request(Net::HTTP::Get.new(uri.request_uri))
         [OK, response.body]
     end
     
     get '/builders' do
-        uri = URI.parse("http://localhost:9090/templates/builders")
+        uri = URI.parse("http://172.31.254.6:8080/templates/builders")
         http = Net::HTTP.new(uri.host, uri.port)
         response = http.request(Net::HTTP::Get.new(uri.request_uri))
         [OK, response.body]
     end
     
     get '/builders/:id' do
-        uri = URI.parse("http://localhost:9090/templates/builders/"+params[:id])
+        uri = URI.parse("http://172.31.254.6:8080/templates/builders/"+params[:id])
         http = Net::HTTP.new(uri.host, uri.port)
         response = http.request(Net::HTTP::Get.new(uri.request_uri))
         [OK, response.body]
     end
     
     get '/provisioners' do
-        uri = URI.parse("http://localhost:9090/templates/provisioners")
+        uri = URI.parse("http://172.31.254.6:8080/templates/provisioners")
         http = Net::HTTP.new(uri.host, uri.port)
         response = http.request(Net::HTTP::Get.new(uri.request_uri))
         [OK, response.body]
     end
     
     get '/provisioners/:id' do
-        uri = URI.parse("http://localhost:9090/templates/provisioners/"+params[:id])
+        uri = URI.parse("http://172.31.254.6:8080/templates/provisioners/"+params[:id])
         http = Net::HTTP.new(uri.host, uri.port)
         response = http.request(Net::HTTP::Get.new(uri.request_uri))
         [OK, response.body]
     end
     
     get '/postprocessors' do
-        uri = URI.parse("http://localhost:9090/templates/postprocessors")
+        uri = URI.parse("http://172.31.254.6:8080/templates/postprocessors")
         http = Net::HTTP.new(uri.host, uri.port)
         response = http.request(Net::HTTP::Get.new(uri.request_uri))
         [OK, response.body]
     end
     
     get '/postprocessors/:id' do
-        uri = URI.parse("http://localhost:9090/templates/postprocessors/"+params[:id])
+        uri = URI.parse("http://172.31.254.6:8080/templates/postprocessors/"+params[:id])
         http = Net::HTTP.new(uri.host, uri.port)
         response = http.request(Net::HTTP::Get.new(uri.request_uri))
         [OK, response.body]
@@ -58,21 +58,21 @@ class PackedImagesApiApp < ApiBase
         if docid.nil?
             body = JSON.parse(request.body.read)
             packed_image = body['packed_image']
-            http = Net::HTTP.new('localhost', 9090)
+            http = Net::HTTP.new('172.31.254.6', 8080)
             response = http.send_request('PUT', '/packer/'+params[:uid],packed_image.to_json)
             PackedImage.create(name: body['name'],doc_id:JSON.parse(response.body)['Id'],org_id:params[:uid])
         else
             if(!params["mciaas_files"].nil?)
-                old_doc = HTTParty.get("http://localhost:9090/packer/"+params[:uid]+"/"+docid)
+                old_doc = HTTParty.get("http://172.31.254.6:8080/packer/"+params[:uid]+"/"+docid)
                 builder = old_doc['builders'][0]#.select{|b| b['type']=="qemu"}.first
                 m_files = {params["mciaas_files"][:filename] => {"type" => "string","content"=> params["mciaas_files"][:tempfile].read}}
                 builder.merge!({"mciaas_files" => m_files})
                 payload = {"builders"=>[builder]}
-                response = HTTParty.post("http://localhost:9090/packer/"+params[:uid]+"/"+docid, :body => payload.to_json)
+                response = HTTParty.post("http://172.31.254.6:8080/packer/"+params[:uid]+"/"+docid, :body => payload.to_json)
             else
                 body = JSON.parse(request.body.read)
                 packed_image = body['packed_image']
-                response = HTTParty.post("http://localhost:9090/packer/"+params[:uid]+"/"+docid, :body => packed_image.to_json)
+                response = HTTParty.post("http://172.31.254.6:8080/packer/"+params[:uid]+"/"+docid, :body => packed_image.to_json)
             end
         end
         [OK, response.body]
@@ -83,7 +83,7 @@ class PackedImagesApiApp < ApiBase
         packed_image = body['packed_image']
         docid = params[:docid]
         response = nil
-        http = Net::HTTP.new('localhost', 9090)
+        http = Net::HTTP.new('172.31.254.6', 8080)
         response = http.send_request('PUT', '/image/'+params[:uid]+'/'+params[:doc_id])
         [OK, response.body]
     end
