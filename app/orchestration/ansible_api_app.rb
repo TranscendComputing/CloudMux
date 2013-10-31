0550638388187dec43a3e7eac2703445b2ebf305
chrrrles <charles.paul@gmail.com>
2013-10-31 16:18:14 -0400

    support for groups, inventories, and hosts POSTing

--- app/orchestration/ansible_api_app.rb
+++ app/orchestration/ansible_api_app.rb
@@ -4,6 +4,11 @@ require 'json'
 
 require File.join(File.dirname(__FILE__),'..','..','lib','ansible','rest_client.rb')
 
+#
+# [XXX] The AWX API supports filtering for get requests... we don't yet
+# [XXX] Don't-Repeat-Yourself! (the rescues in my code are obviously not DRY)
+#
+
 class AnsibleApiApp < ApiBase
 
   before do
@@ -28,48 +33,217 @@ class AnsibleApiApp < ApiBase
     end
   end
 
+  get '/job_templates' do
+    begin
+      results = @ansible.get_job_templates
+      [OK, results.to_json]
+      ## Results
+      #
+      #Each job template data structure includes the following fields:
+      #
+      # `id`: Database ID for this job template. (integer, read-only)
+      # `url`: URL for this job template. (string, read-only)
+      # `related`: Data structure with URLs of related resources. (object, read-only)
+      # `summary_fields`: Data structure with name/description for related resources. (object, read-only)
+      # `created`: Timestamp when this job template was created. (datetime, read-only)
+      # `modified`: Timestamp when this job template was last modified. (datetime, read-only)
+      # `name`:  (string, required)
+      # `description`:  (string)
+      # `job_type`:  (multiple choice, required)
+      # `inventory`:  (field)
+      # `project`:  (field)
+      # `playbook`:  (string, required)
+      # `credential`:  (field)
+      # `forks`:  (integer)
+      # `limit`:  (string)
+      # `verbosity`:  (integer)
+      # `extra_vars`:  (string)
+      # `job_tags`:  (string)
+      # `host_config_key`:  (string)
+    rescue RestClient::Unauthorized
+        [BAD_REQUEST, {:message => "Invalid Ansible user/password combination."}];
+
+    rescue Errno::ECONNREFUSED
+        [BAD_REQUEST, {:message => "Connection was refused"}]
+    end
+  end
+
+# The hierarchy of Ansible goes like this:
+# Inventories -> Groups -> Hosts
+# nevar forget.
   get '/inventories' do 
     begin
-      hosts = @ansible.get_inventories 
-      [OK, hosts.to_json]
+      inventories = @ansible.get_inventories 
+      [OK, inventories.to_json]
+
     rescue RestClient::Unauthorized
         [BAD_REQUEST, {:message => "Invalid Ansible user/password combination."}];
     rescue Errno::ECONNREFUSED
         [BAD_REQUEST, {:message => "Connection was refused"}]
     end
   end
-  get '/job_templates' do
+
+  post '/inventories' do
     begin
-      ## Results
-      #
-      #Each job template data structure includes the following fields:
-      #
-      #* `id`: Database ID for this job template. (integer, read-only)
-      #* `url`: URL for this job template. (string, read-only)
-      #* `related`: Data structure with URLs of related resources. (object, read-only)
-      #* `summary_fields`: Data structure with name/description for related resources. (object, read-only)
-      #* `created`: Timestamp when this job template was created. (datetime, read-only)
-      #* `modified`: Timestamp when this job template was last modified. (datetime, read-only)
-      #* `name`:  (string, required)
-      #* `description`:  (string)
-      #* `job_type`:  (multiple choice, required)
-      #* `inventory`:  (field)
-      #* `project`:  (field)
-      #* `playbook`:  (string, required)
-      #* `credential`:  (field)
-      #* `forks`:  (integer)
-      #* `limit`:  (string)
-      #* `verbosity`:  (integer)
-      #* `extra_vars`:  (string)
-      #* `job_tags`:  (string)
-      #* `host_config_key`:  (string)
-      #
-      results = @ansible.get_job_templates
-      [OK, results.to_json]
+      # Params for inventories post
+      # * `name`:  (string, required)
+      # * `description`:  (string)
+      # * `organization`:  (field, required)
+      # * `variables`: Variables in JSON or YAML format. (string)
+      response = @ansible.post_inventories(
+        :name => params[:name],
+        :description => params[:description],
+        :organization => params[:organization],
+        :variables => params[:variables])
+      [OK, response.to_json]
     rescue RestClient::Unauthorized
         [BAD_REQUEST, {:message => "Invalid Ansible user/password combination."}];
     rescue Errno::ECONNREFUSED
         [BAD_REQUEST, {:message => "Connection was refused"}]
     end
+  end
+
+  get '/groups' do 
+    begin
+      # A groups result from ansible looks like:
+      #  `id`: Database ID for this group. (integer, read-only)
+      #  `url`: URL for this group. (string, read-only)
+      #  `related`: Data structure with URLs of related resources. (object, read-only)
+      #  `summary_fields`: Data structure with name/description for related resources. (object, read-only)
+      #  `created`: Timestamp when this group was created. (datetime, read-only)
+      #  `modified`: Timestamp when this group was last modified. (datetime, read-only)
+      #  `name`:  (string, required)
+      #  `description`:  (string)
+      #  `inventory`:  (field, required)
+      #  `variables`: Variables in JSON or YAML format. (string)
+      #  `has_active_failures`:  (boolean, read-only)
+      groups = @ansible.get_groups
+      [OK, groups.to_json]
+
+    rescue RestClient::Unauthorized
+        [BAD_REQUEST, {:message => "Invalid Ansible user/password combination."}];
+    rescue Errno::ECONNREFUSED
+        [BAD_REQUEST, {:message => "Connection was refused"}]
+    end
+  end
+
+  post '/groups' do
+    begin
+      # Request params expected in AWX:
+      # `name`:  (string, required)
+      # `description`:  (string)
+      # `inventory`:  (field, required)
+      # `variables`: Variables in JSON or YAML format. (string)
+      #
+      response = @ansible.post_groups(
+        :name => params[:name],
+        :description => params[:description],
+        :inventory => params[:inventory],
+        :variables => params[:variables])
+      [OK, response.to_json]
+    rescue RestClient::Unauthorized
+        [BAD_REQUEST, {:message => "Invalid Ansible user/password combination."}];
+    rescue Errno::ECONNREFUSED
+        [BAD_REQUEST, {:message => "Connection was refused"}]
+    end
+  end
+
+  get '/hosts' do 
+    begin
+      hosts = @ansible.get_hosts 
+      [OK, hosts.to_json]
+      #Each host data structure includes the following fields:
+      #
+      # `id`: Database ID for this host. (integer, read-only)
+      # `url`: URL for this host. (string, read-only)
+      # `related`: Data structure with URLs of related resources. (object, read-only)
+      # `summary_fields`: Data structure with name/description for related resources. (object, read-only)
+      # `created`: Timestamp when this host was created. (datetime, read-only)
+      # `modified`: Timestamp when this host was last modified. (datetime, read-only)
+      # `name`:  (string, required)
+      # `description`:  (string)
+      # `inventory`:  (field, required)
+      # `variables`: Variables in JSON or YAML format. (string)
+      # `has_active_failures`:  (boolean, read-only)
+      # `last_job`:  (field)
+      # `last_job_host_summary`:  (field)
+      #
+    rescue RestClient::Unauthorized
+        [BAD_REQUEST, {:message => "Invalid Ansible user/password combination."}];
+    rescue Errno::ECONNREFUSED
+        [BAD_REQUEST, {:message => "Connection was refused"}]
+    end
+  end
+
+  post '/hosts' do
+    begin
+      # [XXX] I need to figure out what the last_job* params are for
+      #
+      # Expected params
+      # `name`:  (string, required)
+      # `description`:  (string)
+      # `inventory`:  (field, required)
+      # `variables`: Variables in JSON or YAML format. (string)
+      #
+      # `last_job`:  (field)
+      # `last_job_host_summary`:  (field)
+      #
+      response = @ansible.post_hosts(
+        :name => params[:name], 
+        :description => params[:description], 
+        :inventory => params[:inventory], 
+        :variables=> params[:variables])
+      [OK, response.to_json]
+
+    rescue RestClient::Unauthorized
+        [BAD_REQUEST, {:message => "Invalid Ansible user/password combination."}];
+
+    rescue Errno::ECONNREFUSED
+        [BAD_REQUEST, {:message => "Connection was refused"}]
+    end
+
+  end
+
+  get '/organizations' do 
+    begin
+      hosts = @ansible.get_organizations
+      [OK, hosts.to_json]
+      ## Results
+      #
+      #Each organization data structure includes the following fields:
+      #
+      # `id`: Database ID for this organization. (integer, read-only)
+      # `url`: URL for this organization. (string, read-only)
+      # `related`: Data structure with URLs of related resources. (object, read-only)
+      # `summary_fields`: Data structure with name/description for related resources. (object, read-only)
+      # `created`: Timestamp when this organization was created. (datetime, read-only)
+      # `modified`: Timestamp when this organization was last modified. (datetime, read-only)
+      # `name`:  (string, required)
+      # `description`:  (string)
+      #
+    rescue RestClient::Unauthorized
+        [BAD_REQUEST, {:message => "Invalid Ansible user/password combination."}];
+    rescue Errno::ECONNREFUSED
+        [BAD_REQUEST, {:message => "Connection was refused"}]
+    end
+  end
+
+  post '/organizations' do
+    begin
+      # Expected params
+      # `name`:  (string, required)
+      # `description`:  (string)
+      response = @ansible.post_organizations(
+        :name => params[:name], 
+        :description => params[:description] )
+      [OK, response.to_json]
+
+    rescue RestClient::Unauthorized
+        [BAD_REQUEST, {:message => "Invalid Ansible user/password combination."}];
+
+    rescue Errno::ECONNREFUSED
+        [BAD_REQUEST, {:message => "Connection was refused"}]
+    end
+
   end
 end
--- lib/ansible/rest_client.rb
+++ lib/ansible/rest_client.rb
@@ -18,16 +18,71 @@ class Ansible
 			)
 		end	
 
+    # [XXX] All of these requests return paged results - no support
+    # for that as of yet.
     def get_job_templates
-      resp = @rest['/api/v1/job_templates'].get
-      job_templates = JSON.parse(resp)["results"]
-      return job_templates
+      resp = @rest['/api/v1/job_templates/'].get
+      # ruby's implicit return
+      JSON.parse(resp)["results"]
     end
 
     def get_inventories
-      resp = @rest['/api/v1/inventories'].get
-      hosts = JSON.parse(resp)["results"]
-      return hosts
+      resp = @rest['/api/v1/inventories/'].get
+      JSON.parse(resp)["results"]
+    end
+
+    def post_inventories(name,description, organization,variables)
+      resp = @rest['/api/v1/hosts'].post({
+        :name => name,
+        :description => description,
+        :organization => organization,
+        :variables => variables
+      })
+      #[XXX] Theoretical what this is at this point - need to see 
+      # actual response
+      JSON.parse(resp)["results"]
+    end
+
+    def get_hosts
+      resp = @rest['/api/v1/hosts'].get
+      JSON.parse(resp)["results"]
+    end
+
+    def post_hosts(name,description, inventory,variables)
+      resp = @rest['/api/v1/hosts'].post({
+        :name => name,
+        :description => description,
+        :inventory => inventory,
+        :variables => variables
+      })
+      JSON.parse(resp)["results"]
+    end
+
+    def get_groups
+      resp = @rest['/api/v1/groups'].get
+      JSON.parse(resp)["results"]
+    end
+
+    def post_groups(name,description, inventory,variables)
+      resp = @rest['/api/v1/groups'].post({
+        :name => name,
+        :description => description,
+        :inventory => inventory,
+        :variables => variables
+      })
+      JSON.parse(resp)["results"]
+    end
+
+    def get_organizations
+      resp = @rest['/api/v1/organizations'].get
+      JSON.parse(resp)["results"]
+    end
+
+    def post_organizations(name,description, inventory,variables)
+      resp = @rest['/api/v1/organizations'].post({
+        :name => name,
+        :description => description })
+      JSON.parse(resp)["results"]
     end
 
   end
