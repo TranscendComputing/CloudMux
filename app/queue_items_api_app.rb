bccdb7877484bc252bad83b74faec11e58890f18
chrrrles <charles.paul@gmail.com>
2013-12-05 10:48:36 -0600

    Ansible Job Queueing

---
+++ app/queue_items_api_app.rb
@@ -0,0 +1,56 @@
+require 'sinatra'
+class QueueItemsApiApp < ApiBase
+  get '/' do
+    queue = QueueItem.all.entries
+    [OK, queue.to_json]
+  end
+
+  get '/:id' do
+    qitem = QueueItem.where(id:params[:id]).first
+    if qitem.nil?
+      [NOT_FOUND, {:message=>"Could not find QueueItem"}.to_json]
+    else
+      [OK, qitem.to_json]
+    end
+  end
+
+  post '/' do
+    data = body_to_json(request)
+    if (data.nil?)
+      error = Error.new.extend(ErrorRepresenter)
+      error.message = "Must give attributes for new QueueItem"
+      [BAD_REQUEST, error.to_json]
+    else
+      qitem = QueueItem.new(data)
+      qitem.save!
+      [OK, qitem.to_json]
+    end
+  end
+
+  put '/' do
+    data = body_to_json(request)
+    if (data.nil?)
+      error = Error.new.extend(ErrorRepresenter)
+      error.message = "Must give attributes for new QueueItem"
+      [BAD_REQUEST, error.to_json]
+    else
+      qitem = QueueItem.new(data)
+      qitem.save!
+      [OK, qitem.to_json]
+    end
+  end
+
+  put '/:id' do 
+    data = body_to_json(request)
+    if (data.nil?)
+      error = Error.new.extend(ErrorRepresenter)
+      error.message = "Must give attributes to update QueueItem"
+      [BAD_REQUEST, error.to_json]
+    else
+      qitem = QueueItem.find(params[:id])
+      qitem.update_attributes! data
+      [OK, qitem.to_json]
+    end
+  end
+
+end
