72ecb5e196cb7b85e77578eb7eb274bb22cda357
Barrett <barretthjones@gmail.com>
2013-12-13 13:51:16 -0600

    renamed update_config_manager_representer to config_manager_representer

--- lib/service/representer/config_manager_representer.rb
+++ lib/service/representer/config_manager_representer.rb
@@ -1,4 +1,4 @@
-module UpdateConfigManagerRepresenter
+module ConfigManagerRepresenter
   include Roar::Representer::JSON
 
   # wrap the fields e.g. { "model_name" : { ...fields... }
@@ -12,5 +12,5 @@ module UpdateConfigManagerRepresenter
   property :path
   property :enabled
   property :auth_properties
-
+  property :source_control_credential, :class=>SourceControlCredential, :extend => SourceControlCredentialRepresenter
 end
