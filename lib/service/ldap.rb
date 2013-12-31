6e14fb2c7cd89da5abb75d4c627e2b2af6b310ad
chrrrles <charles.paul@gmail.com>
2013-12-31 10:18:23 -0600

    proper ldap configuration for connections

--- lib/service/ldap.rb
+++ lib/service/ldap.rb
@@ -1,14 +1,14 @@
 require 'active_ldap'
 
 module LDAP
-  def connect(bind_dn, password)
+  def LDAP.connect
     ActiveLdap::Base.setup_connection( {
       :host => LDAP_HOST,
       :port => LDAP_PORT,
       :base => LDAP_BASE,
-      :method => :ssl,
-      :bind_dn => bind_dn,
-      :password => password})
+      :method => LDAP_METHOD,
+      :bind_dn => LDAP_USER,
+      :password => LDAP_PASSWORD})
     begin
       raise ConnectionError, "Could not connect to the LDAP server at %s:%s " % [LDAP_HOST, LDAP_PORT] unless ActiveLdap::Base.search({:base=>LDAP_BASE, :filter=>"(cn=test connection)"}).is_a? Array
       return ActiveLdap::Base
