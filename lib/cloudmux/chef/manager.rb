9ff31240c3ec737fcb30d197a909c9966ff90e44
Curtis Stewart <cstewart@momentumsi.com>
2014-01-03 10:56:48 -0600

    Updated status method for chef to persist cookbook status correctly

--- lib/cloudmux/chef/manager.rb
+++ lib/cloudmux/chef/manager.rb
@@ -72,10 +72,11 @@ module CloudMux
         cb_model = @model.cookbooks.find_or_create_by(name: name)
         ci_presence = has_ci_presence?(status)
         community = cb_model.community.nil? ? false : cb_model.community
-        cb_model.update_attributes!(
+        cb_model.update_attributes(
           ci_presence: ci_presence,
           community: community,
           status: status)
+        cb_model.save!
       end
     end
   end
