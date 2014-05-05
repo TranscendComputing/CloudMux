8858d1b4601135e00667b630c4dca01479b56760
Barrett Jones <barretthjones@gmail.com>
2014-05-05 14:36:11 -0500

    added ssl_verify_peer to false

--- app/vcloud/vcloud_app.rb
+++ app/vcloud/vcloud_app.rb
@@ -2,20 +2,22 @@ require 'sinatra'
 require 'fog'
 
 class VCloudApp < ResourceApiBase
-	before do
-        cloud_cred = get_creds(params[:cred_id])
-        if cloud_cred.nil?
-        	halt [BAD_REQUEST, "Credentials not found."]
-        else
-	        options = cloud_cred.cloud_attributes.merge(:provider => "vcloud")
-	        puts options
-	        @compute = Fog::Compute::VcloudDirector.new(
-					  :vcloud_director_username => options["vcloud_username"],
-					  :vcloud_director_password => options["vcloud_api_key"],
-					  :vcloud_director_host => options["vcloud_director_url"],
-					  :vcloud_director_show_progress => false
-					)
-			@org = @compute.organizations.get_by_name(options["vcloud_organization"])
-  		end
-	end
+  before do
+    cloud_cred = get_creds(params[:cred_id])
+    if cloud_cred.nil?
+      halt [BAD_REQUEST, 'Credentials not found.']
+    else
+      @compute = Fog::Compute.new(
+        :provider => 'vclouddirector',
+        :vcloud_director_username => cloud_cred.cloud_attributes['vcloud_username'],
+        :vcloud_director_password => cloud_cred.cloud_attributes['vcloud_api_key'],
+        :vcloud_director_host => cloud_cred.cloud_attributes['vcloud_director_url'],
+        :vcloud_director_show_progress => false,
+        :connection_options => {
+          :ssl_verify_peer => false
+        }
+      )
+      @org = @compute.organizations.get_by_name(cloud_cred.cloud_attributes['vcloud_organization'])
+    end
+  end
 end
