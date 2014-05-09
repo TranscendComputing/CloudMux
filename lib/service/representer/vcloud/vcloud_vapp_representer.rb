d0fe16db28be324f6afd2252834e5f3dd4a8573d
Phil <phil@phil-VirtualBox.(none)>
2014-05-09 12:01:42 -0500

    minor added some properties to vapp representer

--- lib/service/representer/vcloud/vcloud_vapp_representer.rb
+++ lib/service/representer/vcloud/vcloud_vapp_representer.rb
@@ -4,6 +4,9 @@ module VCloudVappRepresenter
   property :name
   property :org
   property :vdc
+  property :status
+  property :deployed
+  property :description
 
   attr_accessor :org
   attr_accessor :vdc
